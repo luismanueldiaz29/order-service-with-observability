1cdf7b0bdf2f33a54cd3d5025f06c3a3f1361d8a
Luis Manuel Diaz Sequea <luismaprograma@gmail.com>
2025-04-21 22:34:49 -0500

    first commit, create proyect order

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM eclipse-temurin:17-jre
+
+ADD build/libs/app.ja
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO orders.orders(id, customer_id, order_date, total_amount) VALUES(1, 1, '2025-04-21T23:33:12.130+02:00', 10);
+INSERT INTO orders.orders(id, customer_id, order_date, total_amount) VALUES(2, 2, '2025-04-21T23:33:12.130+02:00', 30);
